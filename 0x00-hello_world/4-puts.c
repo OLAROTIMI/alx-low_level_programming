b4d20c4ea82df593fb7ce262c9bfaf658a249894
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-16 21:39:48 -0700

    program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/* 
+ * main- this is the main fucntion 
+ * return:must alwasy be xero 
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
