f0bf362902728e546ac58b63781c8cdb585f3514
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-19 13:36:46 -0700

    for2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main fucntion
+ * Return: must alwasy be xero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
