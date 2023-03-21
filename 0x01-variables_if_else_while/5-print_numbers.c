3963584d36d6c60e475c7ad8fb7f6a4204af5f5c
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-20 18:10:09 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the main fucntion
+ * Return: must alwasy be xero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
