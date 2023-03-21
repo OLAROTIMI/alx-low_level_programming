21d04315878cb5d4355a0c1f311bfa8de79eeae8
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-20 19:09:53 -0700

    num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this is the main fucntion
+ * Return: must alwasy be xero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= '9' ; i++)
+	{
+		if (i == 9)
+			putchar(i +'0');
+		else
+		{
+			putchar(i +'0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
