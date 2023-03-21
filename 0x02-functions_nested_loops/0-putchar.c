5868069e1e277fc4827ff64f3fdf5506ce09687d
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-21 15:00:08 -0700

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
 /**
- * main - main file 
+ * main - main file
  *
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
+	_putchar('_');
 	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
