05a3201c38df6017d56fb7152decebdfd5f75f68
OLAROTIMI <babawaleolarotimi@gmail.com>
2023-03-24 06:05:37 -0700

    debug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - check s for positie or negatie
+ *
+ * @i- varaible to be checked
+ *
+ * return: alwasy 0
+ */
+void positive_or_negative(int i)
+	if (i < 0)
+		printf("%d id negative", i);
+	else
+		printf("%d is positive", i);
+	return (0);
